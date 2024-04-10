cccbd5eb226cc4136dd3a0bbbf10e54ae20eeb0a
Mo Hank <hank@Hanks-MacBook-Pro.local>
2024-04-10 15:42:01 +0800

    update

--- appearance.lua
+++ appearance.lua
@@ -5,11 +5,8 @@ local module = {}
 -- 主入口方法，在 wezterm.lua 中调用
 function module.apply(config)
     -- 主题
-    config.color_scheme = {
-        ['iTerm2 Default'] = {
-            foreground = '#ffffff',
-        }
-    }
+    config.color_scheme = 'iTerm2 Default'
+
     -- 字体
     -- config.font = wezterm.font('ComicShannsMono Nerd Font', { weight = 'Regular', italic = false})
     config.font = wezterm.font('Hack Nerd Font Mono', { weight = 'Regular', italic = false})
@@ -17,23 +14,24 @@ function module.apply(config)
         'ComicShannsMono Nerd Font',
         'JetBrains Mono'
     }
-    config.font_size = 18.0
+    config.font_size = 14.0
+    config.line_height = 1.2
     -- 背景图
     -- config.window_background_image = '/Users/hank/Pictures/idea/50da68258ia8310fcb6135bdfa49f283.jpeg'
     -- config.window_background_image = '/Users/hank/Pictures/idea/music-life-muzyka-zhizn.jpg'
     config.window_background_image = '/Users/hank/Pictures/bg/正在睡觉的北极狐.jpg'
     config.window_background_image_hsb = {
         -- 亮度乘数 
-        brightness = 0.1,
+        brightness = 0.05,
         -- 色相
         hue = 1.0,
         -- 饱和度
         saturation = 1.0,
     }
     -- 背景透明度
-    config.window_background_opacity = 1.0
+    config.window_background_opacity = 0.9
     -- 文本背景透明度
-    config.text_background_opacity = 0.8
+    config.text_background_opacity = 0.6
 end
 
 return module
--- keymap.lua
+++ keymap.lua
@@ -10,14 +10,14 @@ module.apply = function (config)
     -- 配置 leader 键
     -- config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
     config.keys = {
-            { key = 'RightArrow', mods = 'OPT|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
-            { key = 'DownArrow', mods = 'OPT|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+            -- { key = 'RightArrow', mods = 'OPT|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
+            -- { key = 'DownArrow', mods = 'OPT|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
             -- activate pane selection mode with the default alphabet (labels are "a", "s", "d", "f" and so on)
             { key = '8', mods = 'CTRL', action = act.PaneSelect },
             -- activate pane selection mode with numeric labels
             { key = '9', mods = 'CTRL', action = act.PaneSelect { alphabet = '1234567890', }, },
             -- show the pane selection mode, but have it swap the active and selected panes
             { key = '0', mods = 'CTRL', action = act.PaneSelect { mode = 'SwapWithActive', }, },
-        }
+    }
 end
 return module
