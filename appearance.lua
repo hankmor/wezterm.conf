d644bc6c62b9b2bc55c632d1041ef2d43769191e
Mo Hank <hank@Hanks-MacBook-Pro.local>
2024-04-12 11:47:52 +0800

    update

--- appearance.lua
+++ appearance.lua
@@ -6,28 +6,27 @@ local module = {}
 function module.apply(config)
     -- 主题
     -- light
-    config.color_scheme = 'Yousai (terminal.sexy)'
+    -- config.color_scheme = 'Yousai (terminal.sexy)'
     -- config.color_scheme = 'Github'
     -- config.color_scheme = 'farmhouse-light'
     -- config.color_scheme = 'Green Screen (base16)'
     -- config.color_scheme = 'Silk Light (base16)'
-
     -- night
     -- config.color_scheme = 'iTerm2 Default'
     -- config.color_scheme = 'Dark Pastel'
-    config.color_scheme = 'GitHub Dark'
     -- config.color_scheme = 'iTerm2 Smoooooth'
+    config.color_scheme = 'GitHub Dark Default'
 
     -- 字体
-    -- config.font = wezterm.font('ComicShannsMono Nerd Font', { weight = 'Regular', italic = false})
+    config.font = wezterm.font('ComicShannsMono Nerd Font', { weight = 'Regular', italic = false })
     -- config.font = wezterm.font('Hack Nerd Font Mono', { weight = 'Regular', italic = false})
-    config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular', italic = false })
+    -- config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular', italic = false })
     -- config.font = wezterm.font_with_fallback {
     --     'ComicShannsMono Nerd Font',
     --     'JetBrainsMono Nerd Font',
     --     'Hack Nerd Font Mono',
     -- }
-    config.font_size = 13.0
+    config.font_size = 14.0
     config.line_height = 1.2
     -- 背景图
     -- config.window_background_image = '/Users/hank/Pictures/idea/50da68258ia8310fcb6135bdfa49f283.jpeg'
@@ -35,7 +34,7 @@ function module.apply(config)
     config.window_background_image = '/Users/hank/Pictures/bg/正在睡觉的北极狐.jpg'
     config.window_background_image_hsb = {
         -- 亮度乘数
-        brightness = 0.04,
+        brightness = 0.5,
         -- 色相
         hue = 1.0,
         -- 饱和度
@@ -44,7 +43,7 @@ function module.apply(config)
     -- 背景透明度
     config.window_background_opacity = 1.0
     -- 文本背景透明度
-    config.text_background_opacity = 0.0
+    config.text_background_opacity = 0.6
 end
 
 return module
