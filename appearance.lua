4b19080ddaeb105b46e8876b5e6929adb89a71f1
Mo Hank <hank@Hanks-MacBook-Pro.local>
2024-04-24 22:44:46 +0800

    key config pure

--- appearance.lua
+++ appearance.lua
@@ -4,6 +4,9 @@ local module = {}
 
 -- 主入口方法，在 wezterm.lua 中调用
 function module.apply(config)
+    config.enable_tab_bar = true               -- 去掉tabbar
+    config.hide_tab_bar_if_only_one_tab = true -- 如果只有一个 tab 则隐藏tabbar
+
     -- 主题
     -- light
     -- config.color_scheme = 'Yousai (terminal.sexy)'
@@ -21,31 +24,32 @@ function module.apply(config)
     -- 字体
     -- config.font = wezterm.font('ComicShannsMono Nerd Font', { weight = 'Regular', italic = false })
     -- config.font = wezterm.font('Hack Nerd Font Mono', { weight = 'Regular', italic = false})
-    config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular', italic = false })
+    -- config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular', italic = false })
+    config.font = wezterm.font('Monaco', { weight = 'Regular', italic = false })
     -- config.font = wezterm.font_with_fallback {
     --     'ComicShannsMono Nerd Font',
     --     'JetBrainsMono Nerd Font',
     --     'Hack Nerd Font Mono',
     -- }
-    config.font_size = 13.0
+    config.font_size = 16.0
     config.line_height = 1.2
     -- 背景图
     -- config.window_background_image = '/Users/hank/Pictures/idea/50da68258ia8310fcb6135bdfa49f283.jpeg'
     -- config.window_background_image = '/Users/hank/Pictures/idea/music-life-muzyka-zhizn.jpg'
     -- config.window_background_image = '/Users/hank/Pictures/bg/正在睡觉的北极狐.jpg'
-    config.window_background_image = '/Users/hank/Pictures/bg/xh.jpg'
-    config.window_background_image_hsb = {
-        -- 亮度乘数
-        brightness = 0.1,
-        -- 色相
-        hue = 1.0,
-        -- 饱和度
-        saturation = 1.0,
-    }
+    -- config.window_background_image = '/Users/hank/Pictures/bg/xh.jpg'
+    -- config.window_background_image_hsb = {
+    --     -- 亮度乘数
+    --     brightness = 0.1,
+    --     -- 色相
+    --     hue = 1.0,
+    --     -- 饱和度
+    --     saturation = 1.0,
+    -- }
     -- 背景透明度
-    config.window_background_opacity = 1.0
+    -- config.window_background_opacity = 1.0
     -- 文本背景透明度
-    config.text_background_opacity = 0.8
+    -- config.text_background_opacity = 0.8
 end
 
 return module
