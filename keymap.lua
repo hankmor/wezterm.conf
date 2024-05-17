5a0b46f49e9a5b4885df3e7ba16bb0d90d523755
Mo Hank <hank@Hanks-MacBook-Pro.local>
2024-05-17 18:27:13 +0800

    bind keymap to toggle maximize/normal window

--- keymap.lua
+++ keymap.lua
@@ -1,7 +1,7 @@
 -- 快捷键配置
 local wezterm = require('wezterm')
 local act = wezterm.action
-local M = {}
+local M = { maximized = true } -- auto maximized when startup
 
 M.config = function(config)
   -- 36 is the default, but you can choose a different size.
@@ -18,6 +18,19 @@ M.config = function(config)
     { key = '9', mods = 'CTRL', action = act.PaneSelect { alphabet = '1234567890', }, },
     -- show the pane selection mode, but have it swap the active and selected panes
     { key = '0', mods = 'CTRL', action = act.PaneSelect { mode = 'SwapWithActive', }, },
+    -- allow to toggle maximize/normal window
+    {
+      key = 'm',
+      mods = 'CTRL',
+      action = wezterm.action_callback(function(win)
+        M.maximized = not M.maximized
+        if M.maximized then
+          win:maximize()
+        else
+          win:restore()
+        end
+      end)
+    },
   }
 end
 
