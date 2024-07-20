4689e70cd9c5fc63c5344b6ff3a47255c0303c00
Mo Hank <hank@mbp.local>
2024-07-20 18:38:31 +0800

    update theme and font

--- appearance.lua
+++ appearance.lua
@@ -1,123 +1,126 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local M = {}
 
 M.config = function(config)
-  -- =========================================
-  -- 主题配置
-  -- =========================================
+	-- =========================================
+	-- 主题配置
+	-- =========================================
 
-  -- light
-  -- config.color_scheme = 'Yousai (terminal.sexy)'
-  -- config.color_scheme = 'Github'
-  -- config.color_scheme = 'farmhouse-light'
-  -- config.color_scheme = 'Green Screen (base16)'
-  -- config.color_scheme = 'Silk Light (base16)'
-  -- night
-  -- config.color_scheme = 'iTerm2 Default'
-  -- config.color_scheme = 'Dark Pastel'
-  -- config.color_scheme = 'iTerm2 Smoooooth'
-  config.color_scheme = 'GitHub Dark'
-  -- config.color_scheme = "3024 Night"
+	-- light
+	-- config.color_scheme = 'Yousai (terminal.sexy)'
+	-- config.color_scheme = 'Github'
+	-- config.color_scheme = 'farmhouse-light'
+	-- config.color_scheme = 'Green Screen (base16)'
+	-- config.color_scheme = 'Silk Light (base16)'
+	-- night
+	-- config.color_scheme = 'iTerm2 Default'
+	-- config.color_scheme = 'Dark Pastel'
+	-- config.color_scheme = 'tokyonight'
+	config.color_scheme = "tokyonight"
+	-- config.color_scheme = "tokyonight_storm"
+	-- config.color_scheme = "tokyonight_moon"
+	-- config.color_scheme = "tokyonight_night"
 
-  -- =========================================
-  -- 窗口配置
-  -- =========================================
+	-- config.color_scheme = "GitHub Dark"
 
-  -- 背景图
-  -- config.window_background_image = '/Users/hank/Pictures/idea/music-life-muzyka-zhizn.jpg'
-  -- config.window_background_image = '/Users/hank/Pictures/bg/正在睡觉的北极狐.jpg'
-  -- config.window_background_image = '/Users/hank/Pictures/bg/xh.jpg'
-  -- config.window_background_image_hsb = {
-  --     -- 亮度乘数
-  --     brightness = 0.1,
-  --     -- 色相
-  --     hue = 1.0,
-  --     -- 饱和度
-  --     saturation = 1.0,
-  -- }
-  -- 背景透明度
-  -- config.window_background_opacity = 0.8
-  -- 文本背景透明度
-  -- config.text_background_opacity = 0.8
-  -- config.window_background_opacity = 0.7
-  -- 与 window_background_opacity 结合使用时，配置 macOS 在屏幕上合成窗口时使用的模糊半径量。
-  -- config.macos_window_background_blur = 69
-  config.window_decorations = "RESIZE" -- 配置窗口是否有标题栏和/或可调整大小的边框
-  config.enable_scroll_bar = false
+	-- =========================================
+	-- 窗口配置
+	-- =========================================
 
-  -- 窗口padding 设置为0
-  config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  }
+	-- 背景图
+	config.window_background_image = "/Users/hank/Pictures/bg/zf.png"
+	-- config.window_background_image = "/Users/hank/Pictures/bg/girl.jpg"
+	-- config.window_background_image = "/Users/hank/Pictures/bg/mac.jpg"
+	config.window_background_image_hsb = {
+		-- 亮度乘数
+		brightness = 0.03,
+		-- 色相
+		hue = 1,
+		-- 饱和度
+		saturation = 1,
+	}
+	-- 背景透明度
+	config.window_background_opacity = 1
+	-- 文本背景透明度
+	config.text_background_opacity = 1
+	-- 与 window_background_opacity 结合使用时，配置 macOS 在屏幕上合成窗口时使用的模糊半径量。
+	-- config.macos_window_background_blur = 10
+	config.window_decorations = "RESIZE" -- 配置窗口是否有标题栏和/或可调整大小的边框
+	config.enable_scroll_bar = false
 
-  config.window_frame = {
-    font = config.font,
-    font_size = config.font_size,
-  }
+	-- 窗口padding 设置为0
+	config.window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	}
 
-  -- always maximize window when opening
-  wezterm.on("gui-startup", function()
-    local tab, pane, window = wezterm.mux.spawn_window {}
-    window:gui_window():maximize()
-  end)
+	config.window_frame = {
+		font = config.font,
+		font_size = config.font_size,
+	}
 
-  wezterm.on('window-config-reloaded', function(window, pane)
-    window:toast_notification('wezterm', 'configuration reloaded!', nil, 4000)
-  end)
+	-- always maximize window when opening
+	wezterm.on("gui-startup", function()
+		local tab, pane, window = wezterm.mux.spawn_window({})
+		window:gui_window():maximize()
+	end)
 
-  -- =========================================
-  -- Tabbar 配置
-  -- =========================================
+	wezterm.on("window-config-reloaded", function(window, pane)
+		window:toast_notification("wezterm", "configuration reloaded!", nil, 4000)
+	end)
 
-  -- config.tab_bar_at_bottom = true
-  config.enable_tab_bar = true               -- 去掉tabbar
-  config.hide_tab_bar_if_only_one_tab = true -- 如果只有一个 tab 则隐藏tabbar
-  config.use_fancy_tab_bar = true
-  config.tab_max_width = 26
+	-- =========================================
+	-- Tabbar 配置
+	-- =========================================
 
-  -- =========================================
-  -- Performance
-  -- =========================================
+	-- config.tab_bar_at_bottom = true
+	config.enable_tab_bar = false -- 去掉tabbar
+	config.hide_tab_bar_if_only_one_tab = false -- 如果只有一个 tab 则隐藏tabbar
+	config.use_fancy_tab_bar = false
+	config.tab_max_width = 26
 
-  config.max_fps = 24
-  config.animation_fps = 0
-  config.cursor_blink_ease_in = "Constant"
-  config.cursor_blink_ease_out = "Constant"
+	-- =========================================
+	-- Performance
+	-- =========================================
 
-  config.use_resize_increments = false
+	config.max_fps = 24
+	config.animation_fps = 0
+	config.cursor_blink_ease_in = "Constant"
+	config.cursor_blink_ease_out = "Constant"
+
+	config.use_resize_increments = false
 end
 
 -- =========================================
 -- Support neovim Zen mode
 -- =========================================
 
-wezterm.on('user-var-changed', function(window, pane, name, value)
-  local overrides = window:get_config_overrides() or {}
-  if name == "ZEN_MODE" then
-    -- zenmod will use wezterm plugin to increase font size when start zenmod
-    local incremental = value:find("+")
-    local number_value = tonumber(value)
-    if incremental ~= nil then
-      while (number_value > 0) do
-        window:perform_action(wezterm.action.IncreaseFontSize, pane)
-        number_value = number_value - 1
-      end
-      overrides.enable_tab_bar = false
-    elseif number_value < 0 then
-      -- reset font size when quit zenmode
-      window:perform_action(wezterm.action.ResetFontSize, pane)
-      overrides.font_size = nil
-      overrides.enable_tab_bar = true
-    else
-      -- if no +fontsize configurated, process as absolute font size
-      overrides.font_size = number_value
-      overrides.enable_tab_bar = false
-    end
-  end
-  window:set_config_overrides(overrides)
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	local overrides = window:get_config_overrides() or {}
+	if name == "ZEN_MODE" then
+		-- zenmod will use wezterm plugin to increase font size when start zenmod
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			overrides.enable_tab_bar = false
+		elseif number_value < 0 then
+			-- reset font size when quit zenmode
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+			overrides.enable_tab_bar = true
+		else
+			-- if no +fontsize configurated, process as absolute font size
+			overrides.font_size = number_value
+			overrides.enable_tab_bar = false
+		end
+	end
+	window:set_config_overrides(overrides)
 end)
 
 return M
--- font.lua
+++ font.lua
@@ -2,22 +2,34 @@
 -- 字体配置
 -- =========================================
 
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local M = {}
 
 M.config = function(config)
-  -- config.font = wezterm.font('ComicShannsMono Nerd Font', { weight = 'Regular', italic = false })
-  -- config.font = wezterm.font('Hack Nerd Font Mono', { weight = 'Regular', italic = false})
-  -- config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular', italic = false })
-  -- config.font = wezterm.font('Monaco', { weight = 'Regular', italic = false })
-  config.font = wezterm.font_with_fallback {
-    -- { family = 'Monaco',                  weight = 500, italic = false,       stretch = "Expanded", scale = 1.0 },
-    { family = 'JetBrainsMono Nerd Font',   weight = 500,       italic = false,       stretch = "Expanded", scale = 1.0 },
-    { family = 'Hei',                       weight = 500,       stretch = 'Expanded', scale = 1.0 },
-    { family = 'ComicShannsMono Nerd Font', weight = 'Regular', italic = false,       stretch = "Expanded", scale = 1.0 },
-  }
-  config.font_size = 12.0
-  config.line_height = 1.2
+	-- config.font = wezterm.font('ComicShannsMono Nerd Font', { weight = 'Regular', italic = false })
+	-- config.font = wezterm.font('Hack Nerd Font Mono', { weight = 'Regular', italic = false})
+	-- config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Regular', italic = false })
+	-- config.font = wezterm.font('Monaco', { weight = 'Regular', italic = false })
+	config.font = wezterm.font_with_fallback({
+		{
+			family = "JetBrainsMono Nerd Font",
+			weight = 550,
+			italic = false,
+			stretch = "Expanded",
+			scale = 1.0,
+		},
+		{ family = "Hei", weight = 550, stretch = "Expanded", scale = 1.0 },
+		{
+			family = "ComicShannsMono Nerd Font",
+			weight = "Regular",
+			italic = false,
+			stretch = "Expanded",
+			scale = 1.0,
+		},
+		{ family = "Monaco", weight = 500, italic = false, stretch = "Expanded", scale = 1.0 },
+	})
+	config.font_size = 12.0
+	config.line_height = 1.2
 end
 
 return M
